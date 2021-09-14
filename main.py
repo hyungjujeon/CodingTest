8911f14d6f36403bedf24973b537f1fbd3dac669
hyungjujeon <gudwn1549@gmail.com>
2021-09-14 18:13:41 +0900

    feat: create week4.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import WeeklyChallenge.Answer.week1
 import WeeklyChallenge.Answer.week2
+import WeeklyChallenge.Answer.week4
+import WeeklyChallenge.Answer_Improved.week1_improved
 import WeeklyChallenge.Answer_Improved.week2_improved
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # WeeklyChallenge.Answer.week1.main()
     # WeeklyChallenge.Answer.week2.main()
-    WeeklyChallenge.Answer_Improved.week2_improved.main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    WeeklyChallenge.Answer.week4.main()
+    # WeeklyChallenge.Answer_Improved.week1_improved.main()
+    # WeeklyChallenge.Answer_Improved.week2_improved.main()
