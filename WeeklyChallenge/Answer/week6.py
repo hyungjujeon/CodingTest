7b82cc42140289a0f67b02b5899873b3800e1ae0
hyungjujeon <gudwn1549@gmail.com>
2021-09-16 23:56:35 +0900

    feat: create week6.py

---
+++ WeeklyChallenge/Answer/week6.py
@@ -0,0 +1,36 @@
+def solution(weights, head2head):
+    WEIGHT = 0
+    WIN_COUNT = 1
+    WIN_COUNT_OW = 2
+
+    boxer_info_dict = {num: [weight] for num, weight in enumerate(weights)}
+
+    for i, record in enumerate(head2head):
+        record = list(record)
+        win_count = record.count('W')
+        lose_count = record.count('L')
+        if win_count + lose_count != 0:
+            boxer_info_dict[i].append(win_count/(win_count + lose_count))
+        else:
+            boxer_info_dict[i].append(0)
+
+        search_idx = 0
+        win_count_overweight = 0
+        for _ in range(win_count):
+            opponent_idx = search_idx + record[search_idx:].index('W')
+            my_weight = boxer_info_dict[i][WEIGHT]
+            your_weight = boxer_info_dict[opponent_idx][WEIGHT]
+            if my_weight < your_weight:
+                win_count_overweight += 1
+            search_idx = opponent_idx + 1
+        boxer_info_dict[i].append(win_count_overweight)
+
+    answer = [num[0]+1 for num in sorted(boxer_info_dict.items(), key=lambda x:(x[1][WIN_COUNT], x[1][WIN_COUNT_OW], x[1][WEIGHT], -x[0]))]
+    answer.reverse()
+
+    return answer
+
+def main():
+    input_list = [[[50, 82, 75, 120], ["NLWL", "WNLL", "LWNW", "WWLN"]], [[145, 92, 86], ["NLW", "WNL", "LWN"]], [[60, 70, 60], ["NNN", "NNN", "NNN"]]]
+    for input in input_list:
+        solution(input[0], input[1])
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import WeeklyChallenge.Answer.week1
 import WeeklyChallenge.Answer.week2
 import WeeklyChallenge.Answer.week4
 import WeeklyChallenge.Answer.week5
+import WeeklyChallenge.Answer.week6
 import WeeklyChallenge.Answer_Improved.week1_improved
 import WeeklyChallenge.Answer_Improved.week2_improved
 import WeeklyChallenge.Answer_Improved.week4_improved
@@ -12,7 +13,8 @@ if __name__ == '__main__':
     # WeeklyChallenge.Answer.week2.main()
     # WeeklyChallenge.Answer.week4.main()
     # WeeklyChallenge.Answer.week5.main()
+    WeeklyChallenge.Answer.week6.main()
     # WeeklyChallenge.Answer_Improved.week1_improved.main()
     # WeeklyChallenge.Answer_Improved.week2_improved.main()
     # WeeklyChallenge.Answer_Improved.week4_improved.main()
-    WeeklyChallenge.Answer_Improved.week5_improved.main()
+    # WeeklyChallenge.Answer_Improved.week5_improved.main()
